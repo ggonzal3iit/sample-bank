28b6be80c9c33508daf1a1aeea09217c244a0f88
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2021-08-07 13:01:44 -0500

    update print statement

--- src/org/perscholas/Main.java
+++ src/org/perscholas/Main.java
@@ -3,6 +3,6 @@ package org.perscholas;
 public class Main {
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Welcome to the banking app");
+        System.out.println("Welcome to the banking app. We hope you enjoy your stay!");
     }
 }
